90607317ae0acdb62e71c89956f4f79fb0f0bf83
Pelash <oluwapelumiashabi4@gmail.com>
2025-08-22 17:25:55 +0100

    fix bug in media query

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-let age = 'daniel';
+// VARIABLES
+
+let weight = parseInt(prompt('Enter your weight (kg)'));
+let height = parseFloat(prompt('Enter your height (m)'));
+let gender = prompt('Enter your gender as M or F');
+let bmiCatergory;
+
+// BMI CALCULATE
+let bmi = weight / height ** 2;
+
+// OUTPUT
+console.log(`Your bmi is: ${bmi}`);
