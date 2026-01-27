24b606ea21588d310d402680cf1bef8dfe5f82c3
Pelash <oluwapelumiashabi4@gmail.com>
2026-01-27 23:59:53 +0100

    Refactor CSS and JavaScript structure for improv   ed readability

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // VARIABLES
 
-let weight = parseInt(prompt('Enter your weight (kg)'));
-let height = parseFloat(prompt('Enter your height (m)'));
-let gender = prompt('Enter your gender as M or F');
-let bmiCatergory;
+// let weight = parseInt(prompt('Enter your weight (kg)'));
+// let height = parseFloat(prompt('Enter your height (m)'));
+// let gender = prompt('Enter your gender as M or F');
+// let bmiCatergory;
 
-// BMI CALCULATE
-let bmi = weight / height ** 2;
+// // BMI CALCULATE
+// let bmi = weight / height ** 2;
 
-// OUTPUT
-console.log(`Your bmi is: ${bmi}`);
+// // OUTPUT
+// console.log(`Your bmi is: ${bmi}`);
+
+let counter = 1;
+
+while (counter <= 18) {
+     console.log(counter);
+     counter = counter + 1;
+    
+ }
